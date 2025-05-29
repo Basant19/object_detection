ed585db55f5e8c1bcaa6c4fe7408afc146791309
Basant <btirkey1208@gmail.com>
2025-05-29 23:47:05 +0530

    Forcefully removed yolov5 from staging

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from signLanguage.logger import logging
-from signLanguage.exception import SignException
-import sys
 
+from signLanguage.pipeline.training_pipeline import TrainPipeline
+
+obj = TrainPipeline()
+obj.run_pipeline()
